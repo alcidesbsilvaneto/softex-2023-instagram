334d2e1b2fe33d2593da949d0f591fe022aafa6c
Alcides Bezerra <alcides.bsilvaneto@gmail.com>
2023-11-06 20:52:31 -0300

    Typeorm and web server setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import express from "express";
-import dotenv from "dotenv";
+import { startWebServer } from "./app";
+import { startDatabase } from "./services/database/app-data-source";
 
-dotenv.config();
+async function main() {
+  try {
+    await startDatabase();
+    console.log(`Database initialized`);
+    await startWebServer();
+    console.log(`Web server initialized`);
+  } catch (error) {
+    console.log(error, "Error initializing app");
+  }
+}
 
-const app = express();
-
-app.use(express.json());
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
-});
+main();
