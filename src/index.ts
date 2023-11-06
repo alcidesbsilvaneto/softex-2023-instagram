7f6e35c0bc4999f205c6a903bde846bccd403e13
Alcides Bezerra <alcides.bsilvaneto@gmail.com>
2023-11-06 20:34:08 -0300

    Setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
