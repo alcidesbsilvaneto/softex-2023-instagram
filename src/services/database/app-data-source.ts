b61b334b370cce1d6aa25b136eac54e561e385e5
Alcides Bezerra <alcides.bsilvaneto@gmail.com>
2023-11-06 20:59:42 -0300

    User entity

--- src/services/database/app-data-source.ts
+++ src/services/database/app-data-source.ts
@@ -1,6 +1,6 @@
 import { DataSource } from "typeorm";
+import { User } from "../../modules/users/entities/user.entity";
 
-console.log(process.env.DATABASE_PASSWORD, "process.env.DATABASE_PASSWORD");
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DATABASE_HOST,
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [],
+  entities: [User],
   synchronize: true,
 });
 
